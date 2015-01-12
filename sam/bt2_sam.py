e80dda048df898fb6fa54119f9c19df7a09690c5
Shareef Dabdoub <shareef@dabdoub.net>
2015-01-12 17:33:43 -0500

    Adds type mapping for optional fields

--- sam/bt2_sam.py
+++ sam/bt2_sam.py
@@ -1,3 +1,7 @@
+"""
+This module provides tools for parsing Bowtie2-format SAM files.
+"""
+
 from collections import defaultdict, namedtuple
 
 SAMRecordLite = namedtuple('SAMRecord', 'queryid flags refid startpos mapqual cigar pairid pairstart pairfraglen querylen AS XS YS XN XM XO XG NM YF YT MD')
@@ -13,13 +17,18 @@ def gather_by_query(sam_fp, aligned=True):
     recs = defaultdict(list)
     opt_fields = ['AS', 'XS', 'YS', 'XN', 'XM', 'XO', 
                   'XG', 'NM', 'YF', 'YT', 'MD']
+    opt_type = {'AS': int, 'XS': int, 'YS': int, 'XN': int, 'XM': int, 
+                'XO': int, 'XG': int, 'NM': int, 'YF': str, 'YT': str,
+                'MD': str}
 
     with open(sam_fp, 'rU') as sf:
         split_opt = lambda split_entry: (split_entry[0], split_entry[-1])
         for line in sf:
+            if line[0] == '@':
+                continue
             line = line.split('\t')
             file_opt = dict([split_opt(o.split(':')) for o in line[11:]])
-            full_opt = [file_opt.get(opt, '') for opt in opt_fields]
+            full_opt = [opt_type[opt](file_opt[opt]) if opt in file_opt else '' for opt in opt_fields]
             line = list(line[:9]) + [len(line[9])]
             line.extend(full_opt)
             rec = SAMRecordLite(*line)
